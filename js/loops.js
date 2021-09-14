e7aaa82cf19f8c9ca14da8d690a64abfbf7fb225
Occelli <occelli.hugo@hotmail.fr>
2021-09-14 17:01:44 +0200

    atelier sur les boucles while et for

--- js/loops.js
+++ js/loops.js
@@ -4,10 +4,28 @@ let students = [
     "Théo",
     "Louis",
     "Cécile",
-    "Chloé"
+    "Chloé",
+    "Brandon",
+    "Thimotée"
 ]; 
 
 while (i <= 3){
     console.log(students[i]);
     i++;
 }
+
+// ou
+
+/* while (i <= students.length){
+    console.log(students[i]);
+    i++;
+} */
+
+// ou
+
+for (i=0;i<=students.length-1;i++){
+    console.log(students[i]);
+}
+
+// ou i <= 3 dans les paramètres de la boucle for
+// la boucle for est mieux adaptée pour parcourir les tableaux!
