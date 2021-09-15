f9b05d5f9d5bba3ec92946338953008082e3859e
Occelli <occelli.hugo@hotmail.fr>
2021-09-15 12:07:34 +0200

    for of & for in loops

--- js/loops.js
+++ js/loops.js
@@ -1,4 +1,4 @@
-let i = 0;
+/* let i = 0; */
 
 let students = [
     "Théo",
@@ -9,10 +9,10 @@ let students = [
     "Thimotée"
 ]; 
 
-while (i <= 3){
+/* while (i <= 3){
     console.log(students[i]);
     i++;
-}
+} */ 
 
 // ou
 
@@ -22,10 +22,36 @@ while (i <= 3){
 } */
 
 // ou
-
+/* 
 for (i=0;i<=students.length-1;i++){
     console.log(students[i]);
-}
+} */
 
 // ou i <= 3 dans les paramètres de la boucle for
 // la boucle for est mieux adaptée pour parcourir les tableaux!
+
+/* for (let student of students){
+    console.log(student);
+} */
+
+// la boucle for of n'a pas besoin de compteur, elle le fait automatiquement.
+
+for (let index in students){
+    //console.log(index);
+    // ou
+    console.log("Etudiant numéro : " + (parseInt(index) + 1) + " : " + students[index]); // va chercher les valeurs
+}                                      // ou Number(ind..)
+
+// va chercher l'index (les clés) aulieu de la valeur dans le tableau.
+// for in est plus intéressante/plus puissante.
+
+let human = {
+    "name" : "Henry",
+    "age" : 45,
+    "sexe" : "male",
+    "country" : "France"
+}; // pour un tableau associatif, toujours utilisé la boucle for in !!!!!!
+
+for (let key in human){
+    console.log(key + " : " + human[key]); // pour afficher les clés (key) et ses valeurs (humzn[key])
+}
